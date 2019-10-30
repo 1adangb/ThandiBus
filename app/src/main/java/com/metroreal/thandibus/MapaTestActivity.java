8b821b4b6524ee5360e71b9e00af6f89009e2d73
1adangb <adan.no.adan@gmail.com>
2019-10-30 14:30:57 -0600

    Test Maps

--- app/src/main/java/com/metroreal/thandibus/MapaTestActivity.java
+++ app/src/main/java/com/metroreal/thandibus/MapaTestActivity.java
@@ -8,12 +8,26 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CustomCap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
 
-public class MapaTestActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapaTestActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener, GoogleMap.OnPolygonClickListener {
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+
+    }
 
     private GoogleMap mMap;
+    private static final int COLOR_BLACK_ARGB = 0xff000000;
+    private static final int POLYLINE_STROKE_WIDTH_PX = 12;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +50,65 @@ public class MapaTestActivity extends FragmentActivity implements OnMapReadyCall
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap googleMap)
+    {
+        Polyline polyline1 = googleMap.addPolyline((new PolylineOptions())
+                .clickable(true)
+                .add(
+                        new LatLng(20.638929014946473, -100.4463243484497),
+                        new LatLng(20.64065593024223, -100.44173240661621),
+                        new LatLng(20.640937054132447, -100.44057369232176),
+                        new LatLng(20.640334645160046, -100.44023036956787),
+                        new LatLng(20.639732233801872, -100.43907165527344),
+                        new LatLng(20.639370785841784, -100.43817043304443)));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(20.639370785841784, -100.43817043304443), 8));
+
+
+        // Set listeners for click events.
+        googleMap.setOnPolylineClickListener(this);
+        googleMap.setOnPolygonClickListener(this);
+        //stylePolyline(polyline1);
+
+
+
+
+        //mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    @Override
+    public void onPolygonClick(Polygon polygon) {
+
+    }
+    private void stylePolyline(Polyline polyline) {
+        String type = "";
+        // Get the data object stored with the polyline.
+        if (polyline.getTag() != null) {
+            type = polyline.getTag().toString();
+        }
+
+        switch (type) {
+            // If no type is given, allow the API to use the default.
+            case "A":
+                // Use a custom bitmap as the cap at the start of the line.
+                polyline.setStartCap(
+                        new CustomCap(
+                                BitmapDescriptorFactory.fromResource(R.drawable.ic_arrow), 10));
+                break;
+            case "B":
+                // Use a round cap at the start of the line.
+                polyline.setStartCap(new RoundCap());
+                break;
+        }
+
+        polyline.setEndCap(new RoundCap());
+        polyline.setWidth(POLYLINE_STROKE_WIDTH_PX);
+        polyline.setColor(COLOR_BLACK_ARGB);
+        polyline.setJointType(JointType.ROUND);
+    }
+
 }
