89ee70c020d3d6000c7bd3f32a5f19e75b4fb883
1adangb <adan.no.adan@gmail.com>
2019-12-02 07:01:05 -0600

    agregar lista rutas para pasajero

---
+++ app/src/main/java/com/metroreal/thandibus/RutasPActivity.java
@@ -0,0 +1,64 @@
+package com.metroreal.thandibus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class RutasPActivity extends AppCompatActivity {
+
+    ListView lsRutas;
+    FirebaseFirestore fDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rutas_p);
+        lsRutas = (ListView) findViewById(R.id.lstRutas);
+        fDatabase = FirebaseFirestore.getInstance();
+        llenarListaRutas();
+    }
+
+    private void llenarListaRutas()
+    {
+        fDatabase.collection("ruta").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    ArrayList<QueryDocumentSnapshot> listaRutas = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult())
+                    {
+
+                        listaRutas.add(document);
+                    }
+                    if (listaRutas != null)
+                    {
+                        RutasPAdapter adapter = new RutasPAdapter(RutasPActivity.this,0,listaRutas);
+                        lsRutas.setAdapter(adapter);
+                    }
+                    else
+                    {
+                        Toast.makeText(RutasPActivity.this, "No hay rutas disponibles", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(RutasPActivity.this, "Error al obtener rutas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/metroreal/thandibus/RutasPAdapter.java
@@ -0,0 +1,57 @@
+package com.metroreal.thandibus;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RutasPAdapter extends ArrayAdapter<QueryDocumentSnapshot>
+{
+    private Context mContext;
+    private List<QueryDocumentSnapshot> listaRutas = new ArrayList<>();
+
+
+    public RutasPAdapter(@NonNull Context context, int resource, @NonNull List<QueryDocumentSnapshot> objects)
+    {
+        super(context, resource, objects);
+        mContext = context;
+        listaRutas = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View listItem = convertView;
+        if(listItem == null)
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.list_item,parent,false);
+
+        final QueryDocumentSnapshot estaRuta = listaRutas.get(position);
+
+        TextView txRuta = (TextView) listItem.findViewById(R.id.txtNombreRuta);
+        txRuta.setText(estaRuta.getString("nombreRuta"));
+
+        txRuta.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(mContext,UserMapsActivity.class);
+                intent.putExtra("idRuta",estaRuta.getId());
+                mContext.startActivity(intent);
+            }
+        });
+        return listItem;
+    }
+}
